639efb5e4d5fae9572fac81e079f020b476c323a
TOUBAL Zine-Eddine <zine-eddine.toubal@arteliagroup.com>
2025-03-01 15:39:47 +0100

    feat/create-header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    CommonModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  menuOpen = false;
 
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
 }
