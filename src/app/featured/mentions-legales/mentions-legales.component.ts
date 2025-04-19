a06854d4a30f76cafd3405744db4d95ce8e2256b
TOUBAL Zine-Eddine <zine-eddine.toubal@arteliagroup.com>
2025-04-19 19:14:03 +0200

    feat: mentions-legales

---
+++ src/app/featured/mentions-legales/mentions-legales.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mentions-legales',
+  imports: [],
+  templateUrl: './mentions-legales.component.html',
+  styleUrl: './mentions-legales.component.scss'
+})
+export class MentionsLegalesComponent {
+
+}
--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -3,6 +3,7 @@ import { Footer } from '../../../core/models/footer.model';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
 import { Config } from '../../../core/config/config';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -13,7 +14,7 @@ import { Config } from '../../../core/config/config';
 export class FooterComponent {
   footer!: Footer;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private router: Router) {
     this.getFooter().subscribe(data => {
       this.footer = data.footer;
     });
@@ -22,5 +23,9 @@ export class FooterComponent {
   getFooter(): Observable<{ footer: Footer }> {
     const dataUrl = Config.constantsUrl;
     return this.http.get<{ footer: Footer }>(dataUrl);
+  }
+
+  navigateToMentionsLegales() {
+    this.router.navigate(['/mentions-legales']);
   }
 }
