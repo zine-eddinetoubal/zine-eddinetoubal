c2fa9762016552079b5a5630f270327d4899b880
TOUBAL Zine-Eddine <zine-eddine.toubal@arteliagroup.com>
2025-04-27 12:32:14 +0200

    feat/create page formation & certifications

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './featured/home/home.component';
 import { ContactComponent } from './featured/contact/contact.component';
 import { MentionsLegalesComponent } from './featured/mentions-legales/mentions-legales.component';
+import { FormationComponent } from './featured/formation/formation.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent, },
     { path: 'home', component: HomeComponent },
     { path: 'contact', component: ContactComponent },
     { path: 'mentions-legales', component: MentionsLegalesComponent },
+    { path: 'formation', component: FormationComponent },
     { path: '**', component: HomeComponent }
 ];
---
+++ src/app/featured/formation/formation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formation',
+  imports: [],
+  templateUrl: './formation.component.html',
+  styleUrl: './formation.component.scss'
+})
+export class FormationComponent {
+
+}
