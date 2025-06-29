f3bf0519c6ffc4fbd46d1c769b37e091c7543e08
TOUBAL Zine-Eddine <72252709+toubal77@users.noreply.github.com>
2025-06-29 12:20:45 +0200

    update/ update competences (#29)
    Co-authored-by: TOUBAL Zine-Eddine <zine-eddine.toubal@arteliagroup.com>

--- src/app/core/models/skills.model.ts
+++ src/app/core/models/skills.model.ts
@@ -1,5 +1,6 @@
 export interface Skills {
-    frontend: Record<string, number>;
-    backend: Record<string, number>;
-    tools: Record<string, number>;
-  }
+  frontend: Record<string, number>;
+  backend: Record<string, number>;
+  tools: Record<string, number>;
+  architecture_design: Record<string, number>;
+}
