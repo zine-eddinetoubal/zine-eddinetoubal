c0cf6037cac1532af40bd7ed926bbc90596d7615
TOUBAL Zine-Eddine <72252709+toubal77@users.noreply.github.com>
2025-09-05 17:41:02 +0200

    feat: update eslint (#41)
    Co-authored-by: Zine-Eddine TOUBAL <zine-eddine.toubal@arteliagroup.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,38 @@
-// @ts-check
-const eslint = require("@eslint/js");
-const tseslint = require("typescript-eslint");
-const angular = require("angular-eslint");
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import reactX from 'eslint-plugin-react-x';
+import reactDom from 'eslint-plugin-react-dom';
+import { globalIgnores } from 'eslint/config';
 
-module.exports = tseslint.config(
+export default tseslint.config([
+  // ignorer les fichiers compilés
+  globalIgnores(['.react-router', 'build']),
   {
-    files: ["**/*.ts"],
+    files: ['**/*.{ts,tsx}'],
     extends: [
-      eslint.configs.recommended,
-      ...tseslint.configs.recommended,
-      ...tseslint.configs.stylistic,
-      ...angular.configs.tsRecommended,
+      js.configs.recommended, // regles ESLint de base
+      ...tseslint.configs.recommendedTypeChecked, // regles TS de base
+      ...tseslint.configs.strictTypeChecked, // regles très strictes
+      reactX.configs['recommended-typescript'], // regles react
+      reactDom.configs.recommended, // regles react DOM
+      reactHooks.configs['recommended-latest'], // regles React Hooks
+      reactRefresh.configs.vite, // regles specifiques vite + React refresh
     ],
-    processor: angular.processInlineTemplates,
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        tsconfigRootDir: import.meta.dirname,
+        projectService: true,
+      },
+    },
     rules: {
       "@angular-eslint/directive-selector": [
         "error",
