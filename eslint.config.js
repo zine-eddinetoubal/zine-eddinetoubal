216e6121c257653a4973756d59073f82884d4173
TOUBAL Zine-Eddine <72252709+toubal77@users.noreply.github.com>
2025-09-07 17:02:32 +0200

    update-data-cv (#42)
    * core : update data cv & add helper lineBreakPipe
    
    * feat: add data cv & update cv
    
    * fix: eslint config
    
    ---------
    
    Co-authored-by: Zine-Eddine TOUBAL <zine-eddine.toubal@arteliagroup.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,18 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
-import reactX from 'eslint-plugin-react-x';
-import reactDom from 'eslint-plugin-react-dom';
-import { globalIgnores } from 'eslint/config';
+// @ts-check
+import { configs } from "@eslint/js";
+import { config, configs as _configs } from "typescript-eslint";
+import { configs as __configs, processInlineTemplates } from "angular-eslint";
 
-export default tseslint.config([
-  // ignorer les fichiers compilés
-  globalIgnores(['.react-router', 'build']),
+export default config(
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.ts"],
     extends: [
-      js.configs.recommended, // regles ESLint de base
-      ...tseslint.configs.recommendedTypeChecked, // regles TS de base
-      ...tseslint.configs.strictTypeChecked, // regles très strictes
-      reactX.configs['recommended-typescript'], // regles react
-      reactDom.configs.recommended, // regles react DOM
-      reactHooks.configs['recommended-latest'], // regles React Hooks
-      reactRefresh.configs.vite, // regles specifiques vite + React refresh
+      configs.recommended,
+      ..._configs.recommended,
+      ..._configs.stylistic,
+      ...__configs.tsRecommended,
     ],
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-    languageOptions: {
-      globals: globals.browser,
-      parserOptions: {
-        tsconfigRootDir: import.meta.dirname,
-        projectService: true,
-      },
-    },
+    processor: processInlineTemplates,
     rules: {
       "@angular-eslint/directive-selector": [
         "error",
--- src/app/core/models/header.model.ts
+++ src/app/core/models/header.model.ts
@@ -1,6 +1,5 @@
 export interface Header {
     formation: string | undefined;
     experiences: string | undefined;
-    projet: string | undefined;
     contact: string | undefined;
 }
